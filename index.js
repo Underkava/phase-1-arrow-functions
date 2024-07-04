51dde7813d678a254e24b5de79b67d94c271353e
Samuel Kinyua <samwakash@gmail.com>
2024-07-04 05:29:20 -0700

    master

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide(){
+    return 2000/100;
+}
+divide;
+
+const square = (num) => {
+    return num * num;
+};
+
+console.log(square(5)); 
+
+const add=(num1, num2)=>{
+    return num1+num2;
+};
+console.log(add(3,5));
